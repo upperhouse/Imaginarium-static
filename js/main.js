f0a167544f8bddaf276c85c67c3dd7d8e86489eb
kbleck <kelly@zenman.com>
2015-04-02 17:10:35 -0600

    Style mobile nav

--- js/main.js
+++ js/main.js
@@ -2,4 +2,7 @@ $(document).ready(function(){
 	$('#PageSearchFormButton').click(function(){
 		$('#PageSearchForm').toggleClass('active');
 	});
+	$('.main-head__mobile-menu').click(function(){
+		$('.main-head__nav').toggleClass('active');
+	});
 });
